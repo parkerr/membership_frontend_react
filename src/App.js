970bdf5d08aa6ad8e83f9f1aca9c87847601c922
Richard Parker <richard.parker@barrattplc.co.uk>
2017-01-23 13:59:55 +0000

    Added sidebar logic

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app wrapper">
+      <div id="wrapper">
         { this.renderNavigation() }
         { this.renderBody() }
       </div>
@@ -54,39 +54,19 @@ class App extends Component {
 
   renderNavigation() {
 
-
-
-
-
-
       return (
-          
-          
-<nav className="navbar navbar-default">
-  <div className="container-fluid">
 
-    <div className="navbar-header">
-      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-      </button>
-      <a className="navbar-brand" href="#">Brand</a>
-    </div>
-
-    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul className="nav navbar-nav">
-        <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
+        <div id="sidebar-wrapper">
+            <ul className="sidebar-nav">
+				<li className="sidebar-brand">
+                    <a href="#">
+                        Membership
+                    </a>
+                </li>
                 <li><Link to="/" onClick={ this.showInternalLink }>Home</Link></li>
                 <li><Link to="/members" activeClassName="active" onClick={ this.showInternalLink }>Members</Link></li>
-      </ul>
-    </div>
-  </div>
-</nav>
-          
-
-
+            </ul>
+        </div>
 
       )
 
@@ -95,16 +75,21 @@ class App extends Component {
   renderBody() {
       if (this.state.iframeLink) {
         return (
-          <section className="app__content main-content">
+          <div id="page-content-wrapper">
+            <div className="container-fluid">
             <iframe src={ this.state.iframeLink } style={{height: this.state.bodyHeight, border: 0}} seamless frameBorder="0"></iframe>
-          </section>
+			</div>
+          </div>
         )
       }
       return (
-        <section className="app__content main-content" style={{minHeight: this.state.bodyHeight}}>
-          { this.props.children }
-        </section>
-      )
+		<div id="page-content-wrapper">
+		<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+            <div className="container-fluid">
+                { this.props.children }
+            </div>
+        </div>
+		)
     
   }
 
