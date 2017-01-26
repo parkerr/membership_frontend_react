8ec64d96c1124a5db5135f36a52ff1af412663ae
Richard Parker <rlparker.uk@gmail.com>
2017-01-26 16:32:39 +0000

    Add some sample member data and build the initial show member screen

--- src/members/members.js
+++ src/members/members.js
@@ -1,18 +1,54 @@
 import React, { Component } from 'react';
-
+import { members } from '../sampledata';
+import house from '../img/house.jpg'
 
 class Members extends Component {
 
   constructor(props, context) {
     super(props, context)
-    this.state = {developments: [], loading: true}
+    this.state = {members: [], loading: true}
   }
 
+  componentWillMount() {
+
+      setTimeout(() => {
+        this.setState({...this.state, loading: false, members: members.members})
+      }, 500)
+  }
+  
   
 
   render() {
+      return (
+        <div>
+          <div className="mui-content-wrapper">
+    <div className="mui-row">
+      { this.state.members.map(member => {
+        return (
+          <div key={ member.memberID } className="mui-col-md-4">
+            <div className="mui-panel mui-panel-default">
+              <div className="mui-panel-heading">
+                <a href="#" className="mui-pull-right mui-text-muted"><em className="fa"></em></a>
+              </div>
+              <div className="mui-panel-body mui-text-center">
+                <img src={ house } alt="Contact" className="mui-center-block mui-img-responsive img-circle thumb64"/>
+                <h4>{ member.memberName }</h4>
+                <p>Email:{ member.memberEmail}</p>
+              </div>
+              <div className="mui-panel-footer mui-clearfix">
+             
+              </div>
+            </div>
+          </div>
+        )
+      }) }
+      </div>
+      </div>
+      </div>
+  )
     
-      return <div> hello </div>
+    
+    
   }
 }
 
---
+++ src/sampledata.js
@@ -0,0 +1,72 @@
+export const members = {
+ "members":[
+      {
+        "memberID": "1",
+        "memberName": "Richard",
+        "memberEmail": "richard@richard.com"
+      },{
+        "memberID": "2",
+        "memberName": "Deepster",
+        "memberEmail": "deep@deep.com"
+      },{
+        "memberID": "3",
+        "memberName": "Kuldeep",
+        "memberEmail": "kuldeep@kuldeep.com"
+      },
+      {
+        "memberID": "4",
+        "memberName": "Richard",
+        "memberEmail": "richard@richard.com"
+      },{
+        "memberID": "5",
+        "memberName": "Deepster",
+        "memberEmail": "deep@deep.com"
+      },{
+        "memberID": "6",
+        "memberName": "Kuldeep",
+        "memberEmail": "kuldeep@kuldeep.com"
+      }
+    ]
+  }
+  
+  var tmpmemberData = [
+    {
+      "images": [
+        "http://s44b1808/0cfe0110.png"
+      ],
+      "addressLine1": "1 the street",
+      "addressLine2": "1 the town",
+      "addressLine3": "1 the city",
+      "addressLine4": "abc 1234",
+      "primaryContactNumber": "01211234567",
+      "memberStatus": "C",
+      "bookingBalance": 1000,
+      "memberID":"1"
+    },
+    {
+      "images": [
+        "http://s44b1808/0cfe0110.png"
+      ],
+      "addressLine1": "2 the street",
+      "addressLine2": "2 the town",
+      "addressLine3": "2 the city",
+      "addressLine4": "abc 1234",
+      "primaryContactNumber": "01211234567",
+      "memberStatus": "C",
+      "bookingBalance": 1000,
+      "memberID":"2"
+    },
+    {
+      "images": [
+        "http://s44b1808/0cfe0110.png"
+      ],
+      "addressLine1": "3 the street",
+      "addressLine2": "3 the town",
+      "addressLine3": "3 the city",
+      "addressLine4": "abc 1234",
+      "primaryContactNumber": "01211234567",
+      "memberStatus": "C",
+      "bookingBalance": 1000,
+      "memberID":"3"
+    }
+  ]
