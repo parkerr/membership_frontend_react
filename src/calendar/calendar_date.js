c38ab9d19895e3f68e1d2bf1459020530a9151c3
Richard Parker <rlparker.uk@gmail.com>
2017-01-30 20:59:29 +0000

    Amend

--- src/calendar/calendar_date.js
+++ src/calendar/calendar_date.js
@@ -34,16 +34,16 @@ class Calendar_date extends Component {
           <table width="100%">
           <tbody>
             <tr style={{height:"50px"}}>
-              <td width="20%" className="mui--align-middle mui--text-right">
-                <button type="button" className="mui-col-md-1 mui-col-md-offset-3 mui-btn mui-btn--primary mui-btn--small" onClick={ this.moveback } >
+              <td width="20%" className="mui--align-middle">
+                <button type="button" className="mui-col-xs-6 mui-col-md-4 mui-col-lg-3 mui-col-md-offset-6 mui-col-xs-offset-4 mui-btn mui-btn--primary mui-btn--small" onClick={ this.moveback } >
                   <i className="material-icons md-18 vertical-align-middle padding-bottom-3">chevron_left</i>
                 </button>
               </td>
               <td width="60%" className="mui--align-middle mui--text-center">
                 {this.state.date.toDateString() }
               </td>
-              <td width="20%" className="mui--align-middle mui--text-left">
-                <button className="mui-col-md-1 mui-btn mui-btn--primary mui-btn--small" onClick={ this.moveforward} >
+              <td width="20%" className="mui--align-middle">
+                <button className="mui-col-xs-6 mui-col-md-4 mui-btn--primary mui-col-lg-3 mui-btn mui-btn--primary mui-btn--small" onClick={ this.moveforward} >
                   <i className="material-icons md-18 vertical-align-middle padding-bottom-3">chevron_right</i>
                 </button>
               </td>
